6b30b601096bc55d24f90bd46874b47ffe1832f4
JK-c-w <jyotikhetwal29@gmail.com>
2026-02-25 22:34:20 +0530

    fix: change minification tool from terser to esbuild in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: "dist",
     sourcemap: false,
-    minify: "terser",
+    minify: "esbuild",
     target: "ES2020",
   },
   plugins: plugins,
